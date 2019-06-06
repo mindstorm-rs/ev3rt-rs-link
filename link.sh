fecd6ed718c91eef3fcd60d209e64c800f0a2a5d
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2019-06-06 12:31:37 +0200

    Initial commit.

---
+++ link.sh
@@ -0,0 +1,20 @@
+arm-none-eabi-gcc \
+  -Wl,-nmagic -nostdlib -pie  \
+  -T base-objs/app.ld \
+  -o app \
+  base-objs/module_cfg.o \
+  base-objs/t_perror.o \
+  base-objs/strerror.o \
+  base-objs/vasyslog.o \
+  base-objs/tlsf.o \
+  base-objs/app.o \
+  base-objs/ev3api.o \
+  base-objs/ev3api_battery.o \
+  base-objs/ev3api_brick.o \
+  base-objs/ev3api_fs.o \
+  base-objs/ev3api_speaker.o \
+  base-objs/ev3api_lcd.o \
+  base-objs/ev3api_motor.o \
+  base-objs/ev3api_newlib.o \
+  base-objs/ev3api_sensor.o \
+  -lm -lc -lgcc
