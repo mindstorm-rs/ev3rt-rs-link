0e1318a05176a738639ffbb2fa025c563e7973f6
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2023-03-09 08:46:28 +0100

    Use HRP3 system files.

--- copy-objs.sh
+++ copy-objs.sh
@@ -1,20 +1,19 @@
-cp ../ev3rt-hrp2/base-workspace/uImage firmware
+cp ../ev3rt-hrp3/sdk/firmware/uImage firmware
 cp \
-  ../ev3rt-hrp2/sdk/OBJ/ev3api_battery.o \
-  ../ev3rt-hrp2/sdk/OBJ/ev3api_brick.o \
-  ../ev3rt-hrp2/sdk/OBJ/ev3api_cfg.o \
-  ../ev3rt-hrp2/sdk/OBJ/ev3api_fs.o \
-  ../ev3rt-hrp2/sdk/OBJ/ev3api_lcd.o \
-  ../ev3rt-hrp2/sdk/OBJ/ev3api_motor.o \
-  ../ev3rt-hrp2/sdk/OBJ/ev3api_newlib.o \
-  ../ev3rt-hrp2/sdk/OBJ/ev3api_sensor.o \
-  ../ev3rt-hrp2/sdk/OBJ/ev3api_speaker.o \
-  ../ev3rt-hrp2/sdk/OBJ/ev3api.o \
-  ../ev3rt-hrp2/sdk/OBJ/module_cfg.o \
-  ../ev3rt-hrp2/sdk/OBJ/strerror.o \
-  ../ev3rt-hrp2/sdk/OBJ/tlsf.o \
-  ../ev3rt-hrp2/sdk/OBJ/t_perror.o \
-  ../ev3rt-hrp2/sdk/OBJ/vasyslog.o \
+  ../ev3rt-hrp3/sdk/OBJ/ev3api_battery.o \
+  ../ev3rt-hrp3/sdk/OBJ/ev3api_brick.o \
+  ../ev3rt-hrp3/sdk/OBJ/ev3api_fs.o \
+  ../ev3rt-hrp3/sdk/OBJ/ev3api_lcd.o \
+  ../ev3rt-hrp3/sdk/OBJ/ev3api_motor.o \
+  ../ev3rt-hrp3/sdk/OBJ/ev3api_newlib.o \
+  ../ev3rt-hrp3/sdk/OBJ/ev3api_sensor.o \
+  ../ev3rt-hrp3/sdk/OBJ/ev3api_speaker.o \
+  ../ev3rt-hrp3/sdk/OBJ/ev3api.o \
+  ../ev3rt-hrp3/sdk/OBJ/module_cfg.o \
+  ../ev3rt-hrp3/sdk/OBJ/strerror.o \
+  ../ev3rt-hrp3/sdk/OBJ/tlsf.o \
+  ../ev3rt-hrp3/sdk/OBJ/t_perror.o \
+  ../ev3rt-hrp3/sdk/OBJ/vasyslog.o \
   base-objs
-cp ../ev3rt-hrp2/target/ev3_gcc/dmloader/app/app.ld base-objs
-cp ../ev3rt-hrp2/sdk/OBJ/app.o test-leds
+cp ../ev3rt-hrp3/target/ev3_gcc/dmloader/app/app.ld base-objs
+cp ../ev3rt-hrp3/sdk/OBJ/app.o test-leds
--- link.sh
+++ link.sh
@@ -1,4 +1,4 @@
-cp ../$1/target/armv5te-unknown-linux-gnueabi/release/*.a base-objs/app.a && \
+cp ../$1/target/armv5te-unknown-linux-gnueabi/release/lib$1.a base-objs/app.a && \
 arm-none-eabi-gcc \
   -Wl,-nmagic -nostdlib -pie \
   -T base-objs/app.ld \
@@ -10,7 +10,6 @@ arm-none-eabi-gcc \
   base-objs/tlsf.o \
   base-objs/app.a \
   base-objs/ev3api.o \
-  base-objs/ev3api_cfg.o \
   base-objs/ev3api_battery.o \
   base-objs/ev3api_brick.o \
   base-objs/ev3api_fs.o \
@@ -20,3 +19,11 @@ arm-none-eabi-gcc \
   base-objs/ev3api_newlib.o \
   base-objs/ev3api_sensor.o \
   -lm -lc -lgcc
+
+# arm-none-eabi-gcc
+#   -Wl,-nmagic -nostdlib -pie
+#   -T ../../target/ev3_gcc/dmloader/app/app.ld
+#   -o app
+#   module_cfg.o t_perror.o strerror.o vasyslog.o tlsf.o app.o ev3api.o ev3api_battery.o ev3api_brick.o ev3api_fs.o ev3api_speaker.o ev3api_lcd.o ev3api_motor.o ev3api_newlib.o ev3api_sensor.o
+#   cli_main.o cli_sensor.o cli_motor.o
+#   -lm -lc -lgcc
