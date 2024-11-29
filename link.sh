315ec151ab0f55a511cc7525282d385fa9e8f4b4
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2024-11-29 17:33:23 +0100

    New SDK.

--- link.sh
+++ link.sh
@@ -1,4 +1,33 @@
-cp ../$1/target/armv5te-unknown-linux-gnueabi/release/*.a base-objs/app.a && \
+#/!bin/bash
+
+if [ $# -ne 2 ]
+then
+    echo "Usage: $0 {dir-name} {lib-name}"
+    exit 1
+fi
+
+DIR=$1
+LIB=$2
+
+if [ -d "../$DIR" ]
+then
+    echo $DIR OK
+else
+    echo "$DIR directory not found!"
+    exit 1
+fi
+
+FILE=../$1/target/armv5te-unknown-linux-gnueabi/release/lib$LIB.a
+
+if [ -f "$FILE" ]
+then
+    echo $LIB OK
+else
+    echo "$FILE file not found!"
+    exit 1
+fi
+
+cp $FILE base-objs/app.a && \
 arm-none-eabi-gcc \
   -Wl,-nmagic -nostdlib -pie \
   -T base-objs/app.ld \
@@ -21,3 +50,11 @@ arm-none-eabi-gcc \
   base-objs/ev3api_newlib.o \
   base-objs/ev3api_sensor.o \
   -lm -lc -lgcc
+
+if [ -f app ]
+then
+    file app
+else
+    echo "app file not found!"
+    exit 1
+fi
